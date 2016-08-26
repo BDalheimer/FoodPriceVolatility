d52bcab761f43dc7f361ddbd31da89a71e8224ce
BDalheimer <bernhard.dalheimer@hotmail.com>
2016-08-26 12:09:55 +0200

    aggregated ex-soviet countries and changed policy weights to global maximum

--- R/getIndicators.R
+++ R/getIndicators.R
@@ -1,7 +1,7 @@
 
 getIndicators = function(frequency = "daily", save = FALSE, 
                          type = c("all", "policyTradeWeight", 
-                                  "TradeWeight", "count", "countryIndicators")){
+                                  "TradeWeight", "count", "countryIndicators"), aggregateRussia = F){
 
 library(data.table)
 library(zoo)
@@ -25,35 +25,44 @@ stretchedPolicyData[, iso3c := as.character(countrycode(country_name,
 
 stretchedPolicyData[, period := substring(timeLine,1,4)]
 
-## USDA export data, this needs to be replaced with Trade map data and HS codes, preferrably monthly
-usdaExportData = readRDS("Data/usdaExportData.RDA")
 
+## USDA export data, this needs to be replaced with Trade map data and HS codes, preferrably monthly
+
+if(aggregateRussia == F){
+usdaExportData = readRDS("Data/usdaExportData.RDA")
+}else{
+  usdaExportData = readRDS("Data/usdaExportDataRusAgg.RDA")
+}
 averageExportShares = data.table(aggregate(data = usdaExportData,
                                            exportShare ~ iso3c + period, mean))
 # lag exportshares for exogeneity
-averageExportShares[, period := as.numeric(period)]
-indicatorData = merge(stretchedPolicyData, usdaExportData, by=c("iso3c", "period"), all.x = T)
-indicatorData[, value := as.numeric(value)]
-taxMax = 60
-taxMin = 0
-quotaMax = 4000000
-quotaMin = 0
- indicatorData[, indicatorValues := ifelse(policymeasure_name == "Export quota", value / quotaMax, 
-                                                 ifelse(policymeasure_name == "Export tax", value / taxMax, 
-                                                        ifelse(policymeasure_name == "Export prohibition", 1, 0)))]
-
-#  indicatorData[, indicatorValues := ifelse(policymeasure_name == "Export quota", 2,
-#                                            ifelse(policymeasure_name == "Export tax", 1, 
-#                                                   ifelse(policymeasure_name == "Export prohibition", 3, 0)))]
+averageExportShares[, period := as.numeric(period) + 1]
 
 
-#indicatorData[, indicatorCount := .N, by = timeLine]
-
- # policy and trade weighted Indicator
-policyCount = indicatorData[, .N, by = c("timeLine", "policymeasure_name")]
+# policy and trade weighted Indicator
+policyCount = stretchedPolicyData[, .N, by = c("timeLine", "policymeasure_name")]
 setnames(policyCount, "N", "globalPolicies")
-countryPolicyCount = indicatorData[, .N, by = c("timeLine","policymeasure_name", "iso3c")]
+policyShares = policyCount[, max(globalPolicies), by = "policymeasure_name"]
+setnames(policyShares, "V1", "policyMax")
+countryPolicyCount = stretchedPolicyData[, .N, by = c("timeLine","policymeasure_name", "iso3c")]
 setnames(countryPolicyCount, "N", "countryCount")
+#indicatorData[, indicatorCount := .N, by = timeLine]
+if(aggregateRussia == T){
+  russiaAggregate = data.table(aggregate(data = countryPolicyCount[iso3c %in% c("KAZ", "RUS", "UKR", "KGZ"), ], countryCount ~ timeLine + policymeasure_name, sum))
+  
+  russiaAggregate[, iso3c := "RUK"]
+  
+  countryPolicyCount = rbind(countryPolicyCount[!iso3c %in% c("KAZ", "RUS", "UKR", "KGZ"), ], russiaAggregate)
+}
+ 
+setkey(countryPolicyCount, iso3c, timeLine )
+
+# merge with trade data
+countryPolicyCount[, period := substring(timeLine,1,4)]
+indicatorData = merge(countryPolicyCount, usdaExportData, by=c("iso3c", "period"), all.x = T)
+#indicatorData[, value := as.numeric(value)]
+
+
 quotaBansCount = aggregate(data = countryPolicyCount[policymeasure_name != "Export tax", ], 
                            countryCount  ~ timeLine + iso3c, sum)
 quotaBansGlobal = indicatorData[policymeasure_name != "Export tax", .N, by = c("timeLine")]
@@ -67,11 +76,12 @@ quotaBans = data.table(aggregate(data = quotaBansIndicatorData, quotaBans ~ time
 quotaBansNoPWeight = data.table(aggregate(data = quotaBansIndicatorData, quotaBansNoPWeight ~ timeLine, sum))
 
 countData = merge(countryPolicyCount, policyCount, by = c("timeLine", "policymeasure_name"))
-countData[, period := substring(timeLine,1,4)]
+# countData[, period := substring(timeLine,1,4)]
 newIndicator = merge(countData, usdaExportData, all.x = T, by = c("period", "iso3c"))
+newIndicator = merge(newIndicator, policyShares, all.x = T, by ="policymeasure_name")
 newIndicator[, indicatorNoPoliyWeight := ((countryCount   * exportShare) *100)]
-newIndicator[, indicatorValue := ((countryCount / globalPolicies)   * exportShare) *100]
-
+#newIndicator[, indicatorValue := ((countryCount / globalPolicies)   * exportShare)]
+newIndicator[, indicatorValue := ((countryCount / policyMax)   * exportShare) * 100]
 countryIndicators = dcast(newIndicator, timeLine ~ policymeasure_name + iso3c, value.var = "indicatorValue")
 countryNoPolicyIndicators = dcast(newIndicator, timeLine ~ policymeasure_name + iso3c, value.var = "indicatorNoPoliyWeight")
 globalIndicators = data.table(aggregate(data = newIndicator, indicatorValue ~ timeLine + policymeasure_name, sum))
@@ -82,17 +92,17 @@ globalNoPolicyIndicators = data.table(aggregate(data = newIndicator, indicatorNo
 
 
 quotaValueIndicator = indicatorData[policymeasure_name == "Export quota",]
-countryQuotaIndicators = data.table(aggregate(data = quotaValueIndicator, 
-                                         indicatorValues ~ timeLine + country_name + iso3c + exportShare, sum))
-
-countryQuotaIndicators[, indicatorValues := indicatorValues * 100]
-countryQuotaIndicators[, weightedIndicator := exportShare * indicatorValues * 10000 ]
-
-quotaIndicators = data.table(aggregate(data = countryQuotaIndicators, 
-                                       indicatorValues ~ timeLine, sum))
-
-indicatorDataWide = dcast(globalIndicators, timeLine ~ policymeasure_name, value.var = "indicatorValue")
-test = merge(indicatorDataWide, quotaBans, by = "timeLine", all.x = TRUE)
+# countryQuotaIndicators = data.table(aggregate(data = quotaValueIndicator, 
+#                                          indicatorValues ~ timeLine + country_name + iso3c + exportShare, sum))
+# 
+# countryQuotaIndicators[, indicatorValues := indicatorValues * 100]
+# countryQuotaIndicators[, weightedIndicator := exportShare * indicatorValues * 10000 ]
+# 
+# quotaIndicators = data.table(aggregate(data = countryQuotaIndicators, 
+#                                        indicatorValues ~ timeLine, sum))
+# 
+ indicatorDataWide = dcast(globalIndicators, timeLine ~ policymeasure_name, value.var = "indicatorValue")
+# test = merge(indicatorDataWide, quotaBans, by = "timeLine", all.x = TRUE)
 indicatorDataNoPolicyWide = dcast(globalNoPolicyIndicators, timeLine ~ policymeasure_name, value.var = "indicatorNoPoliyWeight")
 setnames(indicatorDataNoPolicyWide, c("Export prohibition", "Export quota", "Export tax"), c("Export prohibitionNoPweight", "Export quotaNoPweight", "Export taxNoPweight"))
 
@@ -101,25 +111,25 @@ setnames(indicatorDataNoPolicyWide, c("Export prohibition", "Export quota", "Exp
 #setnames(indicatorDataWideExtended, "weightedIndicator", "weightedQuotas")
 
 
-taxValueIndicator = indicatorData[policymeasure_name == "Export tax",]
-
-countryTaxIndicators = data.table(aggregate(data = taxValueIndicator, 
-                                              indicatorValues ~ timeLine + country_name + iso3c + exportShare, sum))
-
-countryTaxIndicators[, indicatorValues := indicatorValues * 100]
-
-
-countryTaxIndicators[, weightedIndicator := exportShare * indicatorValues * 100 ]
-
-taxIndicators = data.table(aggregate(data = countryTaxIndicators, 
-                                       weightedIndicator ~ timeLine, sum))
-setnames(quotaIndicators, "indicatorValues", "measuredQuota")
-setnames(taxIndicators, "weightedIndicator", "measuredTax")
-# library(ggplot2)
+# taxValueIndicator = indicatorData[policymeasure_name == "Export tax",]
 # 
-# ggplot(data=globalIndicators,
-#        aes(x=timeLine, y=indicatorValue, colour=policymeasure_name)) +
-#   geom_line()
+# countryTaxIndicators = data.table(aggregate(data = taxValueIndicator, 
+#                                               indicatorValues ~ timeLine + country_name + iso3c + exportShare, sum))
+# 
+# countryTaxIndicators[, indicatorValues := indicatorValues * 100]
+# 
+# 
+# countryTaxIndicators[, weightedIndicator := exportShare * indicatorValues * 100 ]
+# 
+# taxIndicators = data.table(aggregate(data = countryTaxIndicators, 
+#                                        weightedIndicator ~ timeLine, sum))
+# setnames(quotaIndicators, "indicatorValues", "measuredQuota")
+# setnames(taxIndicators, "weightedIndicator", "measuredTax")
+ library(ggplot2)
+ 
+ggplot(data=globalIndicators,
+       aes(x=timeLine, y=indicatorValue, colour=policymeasure_name)) +
+   geom_line()
 
 indicatorDataWide = merge(indicatorDataWide, quotaBans, by = "timeLine", all.x = TRUE)
 indicatorDataNoPolicyWide = merge(indicatorDataNoPolicyWide, quotaBansNoPWeight, by = "timeLine", all.x = TRUE)
@@ -135,7 +145,10 @@ saveRDS(countryIndicators, "Data/countryIndicators.RDA")
   saveRDS(policyCount, "Data/policyCountMonthly.RDA")
   saveRDS(countryIndicators, "Data/countryIndicatorsMonthly.RDA")
 }
+  
 }
+
+
 if(type == "policyTradeWeight"){
   return(indicatorDataWide)
 }else if(type == "tradeWeight"){
